d1980b0cd9fededa34cbe8c2ef132b9d6b420ea1
Fernando Imparato <ferimpa@gmail.com>
2019-10-22 15:16:41 -0300

    Payments view layout

---
+++ src/views/Payments.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .login-container.column.center.middle
+    router-link.main-button
+    .login.column.start.middle
+
+    h1.title-xbig
+      | Realizar compra
+      .row
+        h1.title-medium.m-top-1.m-right-1
+          | Monto
+        input.input-border.m-bottom-2(
+          type='number'
+          v-model='amountBuy'
+          placeholder='$$$'
+        ).m-right-2
+        h1.title-medium.m-top-1.m-right-1
+          | Establecimiento
+        select.input-border(
+          v-model="valueSelect"
+        )
+          option(v-for="place in selectDropdown", :value="place.value")
+            | {{place.name}}
+      button.main-button.self-end(type='button').m-left-15
+        | Realizar compra
+      
+      h1.title-big
+        | Pagar tarjeta
+      .row
+        h1.title-medium.m-top-1.m-right-2
+          | Monto
+        input.input-border.m-bottom-2(
+          type='number'
+          v-model='amountCreditCardPayment'
+          placeholder='$$$'
+        )
+      button.main-button.self-end(type='button').m-left-15
+          | Pagar tarjeta
+
+    
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      amountBuy: '',
+      selectDropdown: [{name: 'Escuela', value: 1},{name: 'Supermercado', value: 2},{name: 'Gimnasio', value:3}],
+      amountCreditCardPayment: '',
+      valueSelect: 1
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
