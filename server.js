8d61a92e6e54aa218e114eabc8817c739f609876
Laucha <laucha.bo@gmail.com>
2019-10-24 21:37:13 -0300

    fix

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
-
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
-
 const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static(__dirname + "/dist"));
+
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html")
+})
+
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
